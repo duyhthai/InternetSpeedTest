53a73b089bd269b6de88f0a25252efb6ec3b8d3e
Duy Thai <duy.thai@altsourcesoftware.com>
2020-12-14 16:09:54 +0700

    init project

---
+++ InternetSpeedTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InternetSpeedTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new SpeedTest.SpeedTestClient();
+			var server = new SpeedTest.Models.Server();
+
+			// Server list: https://c.speedtest.net/speedtest-servers-static.php
+			server.Url = "http://speedtest3.vtn.com.vn:8080/speedtest/upload.php"; // Ho Chi Minh, VNPT
+
+			var download = client.TestDownloadSpeed(server);
+			var upload = client.TestUploadSpeed(server);
+
+			Console.WriteLine($"Download: {download}");
+			Console.WriteLine($"Upload: {upload}");
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-InternetSpeedTest
+Package
+https://www.nuget.org/packages/SpeedTest.Net/
+
+Speed Test Server List
+https://c.speedtest.net/speedtest-servers-static.php
